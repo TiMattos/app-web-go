c5858f573af119967c5b2dad02e7ec10a615cc4f
thiago mattos <thiagopmattos@hotmail.com>
2023-04-05 22:19:04 -0300

    subindo aula

--- main.go
+++ main.go
@@ -7,11 +7,23 @@ import (
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul top", Preco: 29.90, Quantidade: 4},
+		{"Notebook", "razoavel", 1229.90, 10},
+		{"Fone", "Horrivel", 129.90, 2},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
