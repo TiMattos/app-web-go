65bb4886e1fd1dc978c8193b773af9dfc57ab2f5
thiago mattos <thiagopmattos@hotmail.com>
2023-04-07 11:09:58 -0300

    conexão com o BD utilizando lib pq

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
 	Quantidade int
+}
+
+func conectaComBanco() *sql.DB {
+	connStr := "user=app-web-go password=app-web-go dbname=root host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
 }
 
 func main() {
@@ -20,10 +35,26 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul top", Preco: 29.90, Quantidade: 4},
-		{"Notebook", "razoavel", 1229.90, 10},
-		{"Fone", "Horrivel", 129.90, 2},
+	db := conectaComBanco()
+	retornoConsulta, err := db.Query("select * from produtos")
+
+	if err != nil {
+		panic(err.Error())
 	}
+	produtos := []Produto{}
+
+	for retornoConsulta.Next() {
+		produto := Produto{}
+		err = retornoConsulta.Scan(&produto.Id, &produto.Nome, &produto.Descricao, &produto.Preco, &produto.Quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produtos = append(produtos, produto)
+
+	}
+
 	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
